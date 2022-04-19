b8cd068929fc4e7e17a524a947b97820ae6b1149
awmaker <awmaker@kakao.com>
2022-04-20 04:24:34 +0900

    CLN: remove useless files

--- app/models/index.js
+++ app/models/index.js
@@ -1,11 +1,10 @@
-require('dotenv').config();
-const { MONGO_URI } = process.env;
+require('dotenv').config()
+const { MONGO_URI } = process.env
 const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
 
 const db = {}
 db.mongoose = mongoose
 db.url = MONGO_URI
-db.user = require('./user.model')(mongoose)
 
-module.exports= db
+module.exports = db
